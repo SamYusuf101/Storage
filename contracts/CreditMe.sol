066d3433bb5a05873174ca1e72a6f839e1a7707b
SamYusuf101 <samyusuf101@gmail.com>
2023-10-23 20:41:29 +0100

    updated creditMe

--- contracts/CreditMe.sol
+++ contracts/CreditMe.sol
@@ -1,47 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
-
-
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(
-    uint80 _roundId
-  ) external view returns (
-    uint80 roundId, 
-    int256 answer, 
-    uint256 startedAt, 
-    uint256 updatedAt, 
-    uint80 answeredInRound);
-
-  function latestRoundData()
-    external
-    view
-    returns (
-        uint80 roundId, 
-    int256 answer, 
-    uint256 startedAt, 
-    uint256 updatedAt, 
-    uint80 answeredInRound);
-}
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract CreditMe {
 
-    uint256 public minimumUsd;
-    function fund() public payable{
-       //want to be able to set a minimum amount to be funded
-       require(msg.value >= minimumUsd);
+  uint256 public minimumUsd = 50;
+  address[] public funders;
+  mapping (address => uint256) public amountToNumber;
 
-    }
+  function FundMe() public payable {
+    require(getConvertion(msg.value) >= minimumUsd, "Not Enough");
+    funders.push(msg.sender);
+    //mappin sender to value sent
+    amountToNumber[msg.sender] = msg.value;
 
-    function getVersion() public view returns(uint256){
-       AggregatorV3Interface priceFeed = AggregatorV3Interface (0x694AA1769357215DE4FAC081bf1f309aDC325306);
-       return priceFeed.version();
-    
+  }
 
-}  
+  function getPrice () public view returns(uint256){
+    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    (,int256 price,,,) = priceFeed.latestRoundData();
+    // convert eth in terms of usd
+    return uint256(price * 10e18);
+
+  }
+
+  function getVersion () public view returns(uint256) {
+    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    return priceFeed.version();
+
+  }
+
+  function getConvertion(uint256 ethAmount) public view returns(uint256) {
+    uint256 ethPrice = getPrice();
+    uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+    return ethAmountInUsd;
+  }
 }
--- contracts/StorageBag.sol
+++ contracts/StorageBag.sol
@@ -3,21 +3,23 @@ pragma solidity ^0.8.8;
 
 import "./BasicStorage.sol";
 
-contract StorageBag {
-    BasicStorage[] public basicStorageArray;
-   
-   function CreateContract() public {
-     BasicStorage basicStorage = new BasicStorage();
-     basicStorageArray.push(basicStorage);
+contract Storagebag {
 
-   }
+  BasicStorage[] public basicStorageArray;
 
-   function Store(uint256 _basicStorageIndex, uint256 _basicStorageNumber ) public {
-    basicStorageArray[_basicStorageIndex].store( _basicStorageNumber);
-   }
+  function CreateAddress ()public {
+    BasicStorage basicStorage = new BasicStorage();
+    basicStorageArray.push(basicStorage);
+  }
 
-   function Get(uint256 _basicStorageIndex) public view returns(uint256){
-    return basicStorageArray[_basicStorageIndex].retrieve();
-   }
+  function sfStore(uint256 _basicStorageIndex, uint256 _basicStorageNumber) public  {
+    basicStorageArray[_basicStorageIndex].store(_basicStorageNumber);
+
+  }
+
+function sfGet (uint256 _basicStorageIndex) public view returns(uint256) {
+  return basicStorageArray[_basicStorageIndex].retrieve();
+
+}
 
 }
